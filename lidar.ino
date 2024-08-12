21c2fca5f7752398dc257585af0a201b3238bd4f
GeorgeW-alt2 <gw67656@gmail.com>
2024-08-12 10:17:37 +1000

    Add files via upload

--- lidar.ino
+++ lidar.ino
@@ -3,26 +3,23 @@
 
 SFEVL53L1X distanceSensor;
 
-int previousSignalRate = 0;
 int threshold = 10000; // Threshold to detect a peak
 bool peakDetected = false; // Tracks if a peak was detected
-bool outputOne = false; // Tracks if output 1 should be sent
 int counter = 0;
+int timing = 15;
+bool tri = false;
 void setup(void)
 {
   pinMode(LED_BUILTIN, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH);
   Wire.begin();
-
   Serial.begin(115200);
-
   if (distanceSensor.begin() != 0) // Begin returns 0 on a good init
   {
     while (1)
       ;
   }
 }
-
 void loop(void)
 {
   distanceSensor.startRanging();
@@ -30,26 +27,33 @@ void loop(void)
   {
     delay(1);
   }
-
   int signalRate = distanceSensor.getSignalRate();
   //Serial.println(signalRate);
-
-    if (signalRate < 0 && peakDetected && counter > 0)
+    if (signalRate >= threshold)
     {
-      Serial.write("0"); // Output 0 if there is a dip within 10 cycles
-      peakDetected = false; // Reset peakDetected after detecting the dip
-      counter = 0;
-    }
-
-    if (signalRate >= threshold && counter > 5)
-    {
-        Serial.write("1"); // Output 1 if no dip was detected within 10 cycles
         peakDetected = true; // Reset peakDetected after reporting 1
-        counter = 0;
+        tri = true;
 
     }
-
-  
-
+    if (signalRate < 0 && peakDetected)
+    {
+      peakDetected = false; // Reset peakDetected after detecting the dip
+      tri = true;
+    }
+  if (counter > timing && tri == true){
+     tri = false;
+     if (peakDetected)
+    {
+        Serial.write("1"); // Output 0 if there is a dip within 10 cycles
+        counter = 0;
+    }
+    if (!peakDetected)
+    {
+        Serial.write("0"); // Output 0 if there is a dip within 10 cycles
+        counter = 0;
+    }
+    peakDetected = false; 
+    counter = 0;
+  }
   counter+=1;
 }
